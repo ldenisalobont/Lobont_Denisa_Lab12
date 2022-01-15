2d3b478a124ecea2f1c7c4ca66744c7b00527e01
DENISA-MONICA LOBONŢ <denisa.lobont@stud.ubbcluj.ro>
2022-01-15 12:56:34 +0200

    Lab12

---
+++ Data/WebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lobont_Denisa_Lab12.Models;
+
+namespace WebAPI.Data
+{
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lobont_Denisa_Lab12.Models.ShopList> ShopList { get; set; }
+    }
+}
